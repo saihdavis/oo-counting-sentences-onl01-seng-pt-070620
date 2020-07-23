25a5f7a493878af4b4c41d2573f3e2aba6d5d1ce
Saida Davis <saihdavis@gmail.com>
2020-07-23 20:14:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,7 +28,7 @@ end
 
   def count_sentences
     #binding.pry
-   complex_string = self.split(/.!?/).reject{|string| string.empty?}
+   complex_string = self.split(/[.!?]/).reject{|string| string.empty?}
    complex_string.count
     
   end
